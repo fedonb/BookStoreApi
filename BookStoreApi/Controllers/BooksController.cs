9f59f37aae1aa6b35b1ca12d3630602fb47b0f09
fedonbouzbas <fedon.bouzbas@bossinfo.ch>
2022-01-11 17:46:20 +0200

    Add project files.

---
+++ BookStoreApi/Controllers/BooksController.cs
@@ -0,0 +1,81 @@
+﻿using BookStoreApi.Models;
+using BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly BooksService _booksService;
+
+    public BooksController(BooksService booksService) =>
+        _booksService = booksService;
+
+    [HttpGet]
+    public async Task<List<Book>> Get() =>
+        await _booksService.GetAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        return book;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Book newBook)
+    {
+        await _booksService.CreateAsync(newBook);
+
+        return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Book updatedBook)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        updatedBook.Id = book.Id;
+        Console.WriteLine($"{updatedBook.ToString}");
+        if (updatedBook.BookName == null)
+            updatedBook.BookName = book.BookName;
+        if (updatedBook.Price == 0)
+            updatedBook.Price = book.Price;
+        if (updatedBook.Category == null)
+            updatedBook.Category = book.Category;
+        if (updatedBook.Author == null)
+            updatedBook.Author = book.Author;
+
+        await _booksService.UpdateAsync(id, updatedBook);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        await _booksService.RemoveAsync(book.Id);
+
+        return NoContent();
+    }
+}
---
+++ BookStoreApi/wwwroot/js/site.js
@@ -0,0 +1,122 @@
+﻿const uri = 'api/Books';
+let books = [];
+
+function getItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        .catch(error => console.error('Unable to get books.', error));
+}
+
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+
+    const item = {
+        /*isComplete: false,*/
+        Name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add book.', error));
+}
+
+function deleteItem(id) {
+    fetch(`${uri}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to delete book.', error));
+}
+
+function displayEditForm(id) {
+    const item = books.find(item => item.Id === id);
+
+    document.getElementById('edit-name').value = item.Name;
+    document.getElementById('edit-id').value = item.Id;
+    /*document.getElementById('edit-isComplete').checked = item.isComplete;*/
+    document.getElementById('editForm').style.display = 'block';
+}
+
+function updateItem() {
+    const itemId = document.getElementById('edit-id').value;
+    const item = {
+        Id: parseInt(itemId, 10),
+        /*isComplete: document.getElementById('edit-isComplete').checked,*/
+        Name: document.getElementById('edit-name').value.trim()
+    };
+
+    fetch(`${uri}/${itemId}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to update book.', error));
+
+    closeInput();
+
+    return false;
+}
+
+function closeInput() {
+    document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+    const name = (itemCount === 1) ? 'book' : 'books';
+
+    document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+    const tBody = document.getElementById('books');
+    tBody.innerHTML = '';
+
+    _displayCount(data.length);
+
+    const button = document.createElement('button');
+
+    data.forEach(item => {
+        let name = document.createElement('input');
+        name.type = 'text';
+        name.value = item.Name;
+        name.disabled = true;
+
+        let editButton = button.cloneNode(false);
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayEditForm("${item.Id}")`);
+
+        let deleteButton = button.cloneNode(false);
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteItem("${item.Id}")`);
+
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        let textNode = document.createTextNode(item.Name);
+        td1.appendChild(textNode);
+
+        let td2 = tr.insertCell(1);
+        td2.appendChild(editButton);
+
+        let td3 = tr.insertCell(2);
+        td3.appendChild(deleteButton);
+    });
+
+    books = data;
+}
